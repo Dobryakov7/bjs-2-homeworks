ceb952d898964ea1da12c0f9479ec388d69b6963
Dobryakov Maxim <Friz.09@mail.ru>
2023-03-01 20:24:44 +0300

    HomeWork-2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,22 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let denominator =0;
+  let avg = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i]
+    }
+    if (min > arr[i]) {
+      min = arr[i]
+    }
+    sum += arr[i];
+    denominator = i++;
+  };
+
+  avg = sum/denominator;
 
   return { min: min, max: max, avg: avg };
 }
