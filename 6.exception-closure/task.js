91af8cc81ecaccbd09b77ddbc9f06410dddb511b
Dobryakov Maxim <Friz.09@mail.ru>
2023-06-26 18:07:00 +0300

    HomeWork-6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,57 @@
-﻿
+﻿// Task 1. Number formatter
+
+function parseCount(value) {
+    if (isNaN(Number.parseFloat(value))) {
+      throw new Error("Невалидное значение");
+    }
+    return Number.parseFloat(value);
+  }
+  
+  function validateCount(value) {
+    try {
+      return parseCount(value);
+    } catch (error) {
+      return error;
+    }
+  }
+  
+  
+  // Task 2. Triangle
+  
+  class Triangle {
+    constructor(sideA, sideB, sideC) {
+      this.sideA = sideA;
+      this.sideB = sideB;
+      this.sideC = sideC;
+  
+      if (sideA+sideB<sideC || sideA+sideC<sideB || sideB+sideC<sideA) {
+        throw new Error("Треугольник с такими сторонами не существует");
+      }
+    }
+  
+    get perimeter() {
+      return this.sideA + this.sideB + this.sideC;
+    }
+  
+    get area() {
+      return +Math.sqrt((this.perimeter/2 - this.sideA)*
+      (this.perimeter/2 - this.sideB)*
+      (this.perimeter/2 - this.sideC)*
+      (this.perimeter/2)).toFixed(3);
+    }
+  }
+  
+  function getTriangle(sideA, sideB, sideC) {
+    try {
+      return new Triangle(sideA, sideB, sideC);
+    } catch (error) {
+      return {
+        get perimeter() {
+          return "Ошибка! Треугольник не существует"
+        }, 
+        get area() {
+          return "Ошибка! Треугольник не существует"
+        }
+      };
+    }
+  }
